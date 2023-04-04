31be8f00d16399349ab587cc1a01f2e29a1b3022
kevinthegoose <kevin.christiansen87@gmail.com>
2023-04-04 14:34:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,56 @@
-/* --------------- Spin Wheel  --------------------- */
-const spinWheel = document.getElementById("spinWheel");
-const spinBtn = document.getElementById("spin_btn");
-const text = document.getElementById("text");
-/* --------------- Minimum And Maximum Angle For A value  --------------------- */
-const spinValues = [
-  { minDegree: 61, maxDegree: 90, value: 100 },
-  { minDegree: 31, maxDegree: 60, value: 200 },
-  { minDegree: 0, maxDegree: 30, value: 300 },
-  { minDegree: 331, maxDegree: 360, value: 400 },
-  { minDegree: 301, maxDegree: 330, value: 500 },
-  { minDegree: 271, maxDegree: 300, value: 600 },
-  { minDegree: 241, maxDegree: 270, value: 700 },
-  { minDegree: 211, maxDegree: 240, value: 800 },
-  { minDegree: 181, maxDegree: 210, value: 900 },
-  { minDegree: 151, maxDegree: 180, value: 1000 },
-  { minDegree: 121, maxDegree: 150, value: 1100 },
-  { minDegree: 91, maxDegree: 120, value: 1200 },
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+//Object that stores values of minimum and maximum angle for a value
+const rotationValues = [
+  { minDegree: 0, maxDegree: 30, value: 2 },
+  { minDegree: 31, maxDegree: 90, value: 1 },
+  { minDegree: 91, maxDegree: 150, value: 6 },
+  { minDegree: 151, maxDegree: 210, value: 5 },
+  { minDegree: 211, maxDegree: 270, value: 4 },
+  { minDegree: 271, maxDegree: 330, value: 3 },
+  { minDegree: 331, maxDegree: 360, value: 2 },
 ];
-/* --------------- Size Of Each Piece  --------------------- */
-const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-/* --------------- Background Colors  --------------------- */
-var spinColors = [
-  "#E74C3C",
-  "#7D3C98",
-  "#2E86C1",
-  "#138D75",
-  "#F1C40F",
-  "#D35400",
-  "#138D75",
-  "#F1C40F",
+//Size of each piece
+const data = [16, 16, 16, 16, 16, 16];
+//background color for each piece
+var pieColors = [
+  "#8b35bc",
   "#b163da",
-  "#E74C3C",
-  "#7D3C98",
-  "#138D75",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
 ];
-/* --------------- Chart --------------------- */
-/* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
-let spinChart = new Chart(spinWheel, {
+//Create chart
+let myChart = new Chart(wheel, {
+  //Plugin for displaying text on pie chart
   plugins: [ChartDataLabels],
+  //Chart Type Pie
   type: "pie",
   data: {
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    //Labels(values which are to be displayed on chart)
+    labels: [1, 2, 3, 4, 5, 6],
+    //Settings for dataset/pie
     datasets: [
       {
-        backgroundColor: spinColors,
-        data: size,
+        backgroundColor: pieColors,
+        data: data,
       },
     ],
   },
   options: {
+    //Responsive chart
     responsive: true,
     animation: { duration: 0 },
     plugins: {
+      //hide tooltip and legend
       tooltip: false,
       legend: {
         display: false,
       },
+      //display labels inside pie chart
       datalabels: {
-        rotation: 90,
         color: "#ffffff",
         formatter: (_, context) => context.chart.data.labels[context.dataIndex],
         font: { size: 24 },
@@ -65,36 +58,48 @@ let spinChart = new Chart(spinWheel, {
     },
   },
 });
-/* --------------- Display Value Based On The Angle --------------------- */
-const generateValue = (angleValue) => {
-  for (let i of spinValues) {
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      text.innerHTML = `<p>Congratulations, You Have Won $${i.value} ! </p>`;
+      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
       spinBtn.disabled = false;
       break;
     }
   }
 };
-/* --------------- Spinning Code --------------------- */
+
+//Spinner count
 let count = 0;
+//100 rotations for animation and last rotation for result
 let resultValue = 101;
+//Start spinning
 spinBtn.addEventListener("click", () => {
   spinBtn.disabled = true;
-  text.innerHTML = `<p>Best Of Luck!</p>`;
+  //Empty final value
+  finalValue.innerHTML = `<p>Good Luck!</p>`;
+  //Generate random degrees to stop at
   let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+  //Interval for rotation animation
   let rotationInterval = window.setInterval(() => {
-    spinChart.options.rotation = spinChart.options.rotation + resultValue;
-    spinChart.update();
-    if (spinChart.options.rotation >= 360) {
+    //Set rotation for piechart
+    /*
+    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+    */
+    myChart.options.rotation = myChart.options.rotation + resultValue;
+    //Update chart with new value;
+    myChart.update();
+    //If rotation>360 reset it back to 0
+    if (myChart.options.rotation >= 360) {
       count += 1;
       resultValue -= 5;
-      spinChart.options.rotation = 0;
-    } else if (count > 15 && spinChart.options.rotation == randomDegree) {
-      generateValue(randomDegree);
+      myChart.options.rotation = 0;
+    } else if (count > 15 && myChart.options.rotation == randomDegree) {
+      valueGenerator(randomDegree);
       clearInterval(rotationInterval);
       count = 0;
       resultValue = 101;
     }
   }, 10);
 });
-/* --------------- End Spin Wheel  --------------------- */
