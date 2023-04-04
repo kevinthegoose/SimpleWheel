c55b8ed1378cc38f6fec64b4f8e3f13bbd99e703
kevinthegoose <kevin.christiansen87@gmail.com>
2023-04-04 14:41:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,45 @@ const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
 //Object that stores values of minimum and maximum angle for a value
 const rotationValues = [
-  { minDegree: 0, maxDegree: 30, value: 2 },
-  { minDegree: 31, maxDegree: 90, value: 1 },
-  { minDegree: 91, maxDegree: 150, value: 6 },
-  { minDegree: 151, maxDegree: 210, value: 5 },
-  { minDegree: 211, maxDegree: 270, value: 4 },
-  { minDegree: 271, maxDegree: 330, value: 3 },
-  { minDegree: 331, maxDegree: 360, value: 2 },
+  { minDegree: 0, maxDegree: 18, value: 10 },
+  { minDegree: 19, maxDegree: 36, value: 9 },
+  { minDegree: 37, maxDegree: 54, value: 8 },
+  { minDegree: 55, maxDegree: 72, value: 7 },
+  { minDegree: 73, maxDegree: 90, value: 6 },
+  { minDegree: 91, maxDegree: 108, value: 5 },
+  { minDegree: 109, maxDegree: 126, value: 4 },
+  { minDegree: 127, maxDegree: 144, value: 3 },
+  { minDegree: 145, maxDegree: 162, value: 2 },
+  { minDegree: 163, maxDegree: 180, value: 1 },
+  { minDegree: 181, maxDegree: 198, value: 20 },
+  { minDegree: 199, maxDegree: 216, value: 19 },
+  { minDegree: 217, maxDegree: 234, value: 18 },
+  { minDegree: 235, maxDegree: 252, value: 17 },
+  { minDegree: 253, maxDegree: 270, value: 16 },
+  { minDegree: 271, maxDegree: 288, value: 15 },
+  { minDegree: 289, maxDegree: 306, value: 14 },
+  { minDegree: 307, maxDegree: 324, value: 13 },
+  { minDegree: 325, maxDegree: 342, value: 12 },
+  { minDegree: 343, maxDegree: 360, value: 11 },
 ];
 //Size of each piece
-const data = [16, 16, 16, 16, 16, 16];
+const data = new Array(20).fill(18);
 //background color for each piece
 var pieColors = [
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
   "#8b35bc",
   "#b163da",
   "#8b35bc",
@@ -30,7 +57,7 @@ let myChart = new Chart(wheel, {
   type: "pie",
   data: {
     //Labels(values which are to be displayed on chart)
-    labels: [1, 2, 3, 4, 5, 6],
+    labels: Array.from({ length: 20 }, (_, i) => i + 1),
     //Settings for dataset/pie
     datasets: [
       {
@@ -48,7 +75,7 @@ let myChart = new Chart(wheel, {
       tooltip: false,
       legend: {
         display: false,
-      },
+      },,
       //display labels inside pie chart
       datalabels: {
         color: "#ffffff",
